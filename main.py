3b589c34acc7fe478d05f9747b6c45f18871e9e1
Solomon <bibinusolomon@gmail.com>
2025-01-30 23:15:36 +0100

    updated code

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 from datetime import datetime
 import pytz
 from flask_cors import CORS
-from collections import OrderedDict
+import json
+
 
 app = Flask(__name__)
 
@@ -11,14 +12,16 @@ CORS(app)
 @app.route("/")
 
 def home():
-    response = OrderedDict({
+    response = {
         "email" : "bibinusolomon@gmail.com",
         "current_datetime": datetime.now(pytz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url" : "https://github.com/N-anle/flask-api-HNG12-internship"
 
-    })
+    }
 
-    return jsonify(response)    
+    json_response = json.dumps(response, ensure_ascii=False)
+    
+    return response(json_response, content_type="application/json")
 
 
 if __name__ == "__main__":
