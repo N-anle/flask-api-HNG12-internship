45c5a6f8b420ea880f0c3ac7c7027026876e8354
Solomon <bibinusolomon@gmail.com>
2025-01-30 12:04:37 +0100

    Initial commit: Added flask API code

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+
+def home():
+    response = {
+        "email" : "bibinusolomon@gmail.com",
+        "current_datetime" : datetime.now(pytz.UTC).isoformat(),
+        "github_url" : "https://github.com/N-anle/flask-api-HNG12-internship"
+
+    }
+
+    return jsonify(response)    
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
