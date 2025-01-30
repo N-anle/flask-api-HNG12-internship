15f57d9f5d92adb5a97c05fc417a35a2756109df
Solomon <bibinusolomon@gmail.com>
2025-01-30 23:26:04 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response
 from datetime import datetime
 import pytz
 from flask_cors import CORS
@@ -12,16 +12,16 @@ CORS(app)
 @app.route("/")
 
 def home():
-    response = {
+    response_dict = {
         "email" : "bibinusolomon@gmail.com",
         "current_datetime": datetime.now(pytz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url" : "https://github.com/N-anle/flask-api-HNG12-internship"
 
     }
 
-    json_response = json.dumps(response, ensure_ascii=False)
-    
-    return response(json_response, content_type="application/json")
+    response_json = json.dumps(response_dict, ensure_ascii=False)
+
+    return Response(response_json, mimetype="application/json") 
 
 
 if __name__ == "__main__":
